7913056ad826157e745c5064b83610d583e859e9
Hans-Christian Esperer <hc@hcesperer.org>
2011-02-13 17:44:15 +0100

    Even more optimisations.

--- antwort/zensus_helpers/zensus_dbpool.erl
+++ antwort/zensus_helpers/zensus_dbpool.erl
@@ -2,10 +2,10 @@
 
 -export([start/0]).
 
+-export([cache/1]).
+
 start() ->
     spawn(fun() ->
-		  ets:new(zensus_cache, [set, public,
-					 named_table]),
 		  pgsql_pool:start_link(zensus, 1,
 					[{host, "127.0.0.1"},
 					 {port, 5432},
@@ -15,5 +15,23 @@ start() ->
 					 {schema, "public"}]),
 		  receive after infinity ->
 				  ok end
-	  end).
+	  end),
+    P = spawn(?MODULE, cache, [dict:new()]),
+    register(zensus_cache, P).
+
+cache(Dict) ->
+    receive
+	{insert, Key, Value} ->
+	    ?MODULE:cache(dict:store(Key, Value, Dict));
+	{get, P, Key} ->
+	    try dict:fetch(Key, Dict) of
+		Value ->
+		    P ! [Value]
+	    catch _:_ ->
+		    P ! []
+	    end,
+	    ?MODULE:cache(Dict);
+	_Else ->
+	    ?MODULE:cache(Dict)
+    end.		
 
